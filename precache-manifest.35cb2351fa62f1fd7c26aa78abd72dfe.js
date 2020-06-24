c58780f2dacefc2fa222e1f2a31ac512b1e8e064
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2020-06-24 22:24:48 +0530

    Add files via upload

---
+++ precache-manifest.35cb2351fa62f1fd7c26aa78abd72dfe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4094f05a2cab548e012f93996c0d76dd",
+    "url": "/covid19_website/index.html"
+  },
+  {
+    "revision": "36ccade474c6ae3ccd48",
+    "url": "/covid19_website/static/css/main.21ef6519.chunk.css"
+  },
+  {
+    "revision": "1c2c008963106076fe40",
+    "url": "/covid19_website/static/js/2.010e8b30.chunk.js"
+  },
+  {
+    "revision": "af5111307236c4c69d6adba51f98588a",
+    "url": "/covid19_website/static/js/2.010e8b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ccade474c6ae3ccd48",
+    "url": "/covid19_website/static/js/main.5ddb3203.chunk.js"
+  },
+  {
+    "revision": "86dfe640537c20875f0a",
+    "url": "/covid19_website/static/js/runtime-main.73bafb00.js"
+  },
+  {
+    "revision": "c46821744260d59de9bb4e11ff4b7f47",
+    "url": "/covid19_website/static/media/Covid.c4682174.jpg"
+  },
+  {
+    "revision": "ed70bac8d24835d94d5115395d302ada",
+    "url": "/covid19_website/static/media/Covid19.ed70bac8.jpg"
+  },
+  {
+    "revision": "4b1b305f33ed8ec8ae233cd7cb71033f",
+    "url": "/covid19_website/static/media/Mask.4b1b305f.jpg"
+  },
+  {
+    "revision": "004e44d6b34430893babccafdfe40330",
+    "url": "/covid19_website/static/media/Safe.004e44d6.jpg"
+  },
+  {
+    "revision": "4066a52bac75abb908bfeaec61975dfb",
+    "url": "/covid19_website/static/media/wash.4066a52b.jpg"
+  }
+]);
